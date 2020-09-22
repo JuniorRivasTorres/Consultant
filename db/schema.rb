ddd9abbd8c6c1eb3421fdfc73033aff47cb2540e
Junior RivasTorres <jrtimmobilier@gmail.com>
2020-09-22 09:04:32 -0400

    change to active record

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_21_131240) do
+ActiveRecord::Schema.define(version: 2020_09_21_133420) do
 
   create_table "subjects", force: :cascade do |t|
     t.string "name"
